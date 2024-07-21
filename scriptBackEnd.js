27ae475081d84e0e48b2369d6611b79acb5f8722
Vitor Gabriel Siega <vitorsiega9123@gmail.com>
2024-07-21 20:17:10 -0300

    Front Ver 0.4

--- scriptBackEnd.js
+++ scriptBackEnd.js
@@ -6,6 +6,13 @@ var dataPessoa = document.getElementById("dataPessoa");
 var submitcad = document.getElementById("btcad");
 
 submitcad.addEventListener("click", function(event){
+    event.preventDefault();
+    
+    if (nomePessoa.value.trim() === "" || emailPessoa.value.trim() === "" || senhaPessoa.value.trim() === "" || dataPessoa.value.trim() === "") {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
     fetch("http://localhost:8080/usuarios/cadastro",{
         headers:{
             "Accept": "application/json",
@@ -31,4 +38,49 @@ submitcad.addEventListener("click", function(event){
     senhaPessoa.value = '';
     dataPessoa.value = '';
 
+});
+
+
+//------------------------------------------------------
+
+
+var submitLogin = document.getElementById("loginButton");
+var loginEmail = document.getElementById("loginEmail");
+var loginPass = document.getElementById("loginPassword");
+
+submitLogin.addEventListener("click", function(event){
+    event.preventDefault();
+
+    if (loginEmail.value.trim() === "" || loginPass.value.trim() === "") {
+        alert("Por favor, preencha ambos os campos de email e senha.");
+        return;
+    }
+
+    fetch("http://localhost:8080/api/login", {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            email: loginEmail.value,
+            senha: loginPass.value
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log("Login bem-sucedido");
+            alert("Login bem-sucedido!");
+        } else if (response.status === 401) {
+            console.log("Falha no login. Usuário ou senha incorretos.");
+            alert("Falha no login. Usuário ou senha incorretos.");
+        } else {
+            console.log("Erro ao tentar fazer login.");
+            alert("Erro ao tentar fazer login.");
+        }
+    })
+    .catch(error => {
+        console.error("Erro ao tentar fazer login:", error);
+        alert("Erro ao tentar fazer login. Verifique sua conexão com a internet.");
+    });
 });
--- scriptTrocaTela.js
+++ scriptTrocaTela.js
@@ -2,10 +2,14 @@ document.getElementById('cadastreSeLink').addEventListener('click', function(eve
     event.preventDefault();
     document.getElementById('loginForm').classList.remove('active');
     document.getElementById('cadastroForm').classList.add('active');
+    document.getElementById('titulo').setAttribute("Login");
 });
 
 document.getElementById('voltarLoginLink').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('cadastroForm').classList.remove('active');
     document.getElementById('loginForm').classList.add('active');
+    document.getElementById('titulo').setAttribute("Cadastro");
 });
+
+
