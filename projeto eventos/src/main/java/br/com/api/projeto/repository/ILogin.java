a69ebb5efb57a964350ab89e216fa6ac7faa3786
Vitor Gabriel Siega <vitorgsiega@gmail.com>
2024-07-21 02:08:44 -0300

    Delete projeto eventos directory

--- projeto eventos/src/main/java/br/com/api/projeto/repository/ILogin.java
+++
@@ -1,9 +0,0 @@
-package br.com.api.projeto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.api.projeto.model.Login;
-
-public interface ILogin extends JpaRepository<Login, Long> {
-
-}
--- projeto eventos/src/main/java/br/com/api/projeto/repository/IPessoa.java
+++
@@ -1,9 +0,0 @@
-package br.com.api.projeto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.api.projeto.model.Pessoa;
-
-public interface IPessoa extends JpaRepository<Pessoa, Long> {
-
-}
